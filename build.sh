24af55ad8482e62e33cb366aa545db898d454edb
Zero1366 <cdenaes@mailo.com>
2026-07-06 20:12:57 +0200

    Restaure build stable Render

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py setup_render_data
-python manage.py createsuperuser --noinput || true
+Set-Content -Path .\build.sh -Encoding UTF8 -Value @(
+"pip install -r requirements.txt",
+"",
+"python manage.py collectstatic --noinput",
+"python manage.py migrate --noinput",
+"python manage.py createsuperuser --noinput || true"
+)
