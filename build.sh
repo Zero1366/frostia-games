1992460ca5f77a9feaa3fb5dbb0c53d86ce1267c
Zero1366 <cdenaes@mailo.com>
2026-07-06 20:15:52 +0200

    Corrige script build Render

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-Set-Content -Path .\build.sh -Encoding UTF8 -Value @(
-"pip install -r requirements.txt",
-"",
-"python manage.py collectstatic --noinput",
-"python manage.py migrate --noinput",
-"python manage.py createsuperuser --noinput || true"
-)
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+python manage.py createsuperuser --noinput || true
