16b3986f050f04f0292e598001b1ef8089469365
Zero1366 <cdenaes@mailo.com>
2026-09-04 13:59:43 +0200

    Fix forms.py - syntaxe ModelForm cassait le deploiement

--- creations/forms.py
+++ creations/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Category
 
 
-class CategoryForm(forms.ModelForm[Category]):
+class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ["name", "slug"]
